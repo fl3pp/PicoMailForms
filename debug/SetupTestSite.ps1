11ab98f994aa0da4e84a0a37cd0854770705a555
Jann Flepp <flja@hotmail.ch>
2018-11-19 08:42:21 +0100

    Added first version

---
+++ debug/SetupTestSite.ps1
@@ -0,0 +1,18 @@
+# Set up test website and install PicoMailFormsPlugin
+
+$appDirectory = 'TestApp';
+
+$appDirectory = "$PSScriptRoot\..\..\$appDirectory";
+
+if (Test-Path $appDirectory) {
+    Remove-Item -Recurse -Force $appDirectory;
+}
+New-Item -ItemType Directory $appDirectory;
+Copy-Item $PSScriptRoot\debug\index.md $appDirectory\content\index.md
+
+cd $appDirectory;
+
+composer create-project picocms/pico-composer .
+composer require jflepp/picomailformsplugin
+
+php -S localhost:80
---
+++ debug/index.md
@@ -0,0 +1,7 @@
+# i am a form
+
+[form]
+    [subject]test[/subject]
+    [text]Name[/text]
+    [text]Username[/text]
+[/form]
